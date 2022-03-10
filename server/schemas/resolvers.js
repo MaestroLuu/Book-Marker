69bea3234e93534ebed7673079f0e16f0dd3259b
David Luu <dluu.audeo@gmail.com>
2022-03-09 20:57:43 -0800

    testing savebook mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { Book, User } = require('../models');
-const { authMiddleware, signToken} = require('../utils/auth')
-const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
@@ -8,31 +8,47 @@ const resolvers = {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
       }
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError("No user found with this email address");
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
       const token = signToken(user);
       return { token, user };
     },
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({username, email, password});
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
-      return {token, user};
+      return { token, user };
     },
-    saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
-      const user = await Use.findOne({ token });
-
-    }
+    saveBook: async (parent, {authors, description, bookId, image, link, title} , context) => {
+      console.log(context.user)
+      if (context.user) {
+        return await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: { 
+            authors,
+            description,
+            bookId,
+            image,
+            link,
+            title } } },
+          {
+            new: true,
+            runValidators: true
+          }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in to save your books!");
+    },
   },
 };
 
